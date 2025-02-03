2cf3b3eaad08a78c338d9b6846d72ffdc5052493
Royal Lachinov <royal@actoapp.com>
2025-02-02 22:38:06 -0500

    Initial Commit

---
+++ NetworkConnectivity/NetworkConnectivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkConnectivityApp.swift
+//  NetworkConnectivity
+//
+//  Created by Royal Lachinov on 2025-02-02.
+//
+
+import SwiftUI
+
+@main
+struct NetworkConnectivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
